a797b1c6507295d9fe5836ffd60d9e3a1244a0df
xriser <xtmprz@gmail.com>
2018-06-28 15:29:46 +0300

    post to Blynk today and month usage

--- boiler_mq2db.py
+++ boiler_mq2db.py
@@ -5,6 +5,14 @@ from influxdb.client import InfluxDBClientError
 import configparser
 import os
 import sys
+#from apscheduler.scheduler import Scheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+import re
+import requests
+import json
+
+import datetime
+
 
 
 prev_temp = 0
@@ -17,10 +25,10 @@ config = configparser.ConfigParser()
 config.read(dir_path + "/" + 'boiler_mq2db.conf', encoding="utf8")
 sections = config.sections()
 
-InfluxDBHost = config['settings']['InfluxDBHost']
-InfluxDBPort = config['settings']['InfluxDBPort']
-InfluxDBUser = config['settings']['InfluxDBUser']
-InfluxDBPass = config['settings']['InfluxDBPass']
+InfluxDBHost     = config['settings']['InfluxDBHost']
+InfluxDBPort     = config['settings']['InfluxDBPort']
+InfluxDBUser     = config['settings']['InfluxDBUser']
+InfluxDBPass     = config['settings']['InfluxDBPass']
 InfluxDBDatabase = config['settings']['InfluxDBDatabase']
 
 MqttHost = config.get('settings', 'MqttHost')
@@ -28,6 +36,82 @@ MqttPort = config.getint('settings', 'MqttPort')
 MqttUser = config.get('settings', 'MqttUser')
 MqttPass = config.get('settings', 'MqttPass')
 
+BlynkUrl    = config.get('settings', 'BlynkUrl')
+BlynkPort   = config.get('settings', 'BlynkPort')
+BlynkToken  = config.get('settings', 'BlynkToken')
+
+def load_dirty_json(dirty_json):
+    regex_replace = [(r"([ \{,:\[])(u)?'([^']+)'", r'\1"\3"'), (r" False([, \}\]])", r' false\1'), (r" True([, \}\]])", r' true\1')]
+    for r, s in regex_replace:
+        dirty_json = re.sub(r, s, dirty_json)
+        clean_json = json.loads(dirty_json)
+    return clean_json
+
+
+def post_2blynk(pin, pin_value):
+    print("Check response...")
+    try:
+        response = requests.get(BlynkUrl + ':' + BlynkPort + '/' + BlynkToken + '/update/' + pin + '?value='+ pin_value, timeout=(15, 15))
+        response.raise_for_status()
+
+    except requests.exceptions.ReadTimeout:
+        print('Oops. Read timeout occured')
+
+    except requests.exceptions.ConnectTimeout:
+        print('Oops. Connection timeout occured!')
+
+    except requests.exceptions.ConnectionError:
+        print('Seems like dns lookup failed..')
+
+    except requests.exceptions.HTTPError as err:
+        print('Oops. HTTP Error occured')
+        print('Response is: {content}'.format(content=err.response.content))
+
+    print("Response status code: " + str(response.status_code))
+
+    text = response.text
+
+
+def every_minute():
+    print("Every minute")
+
+    d = datetime.date.today()
+    month = '{:02d}'.format(d.month)
+    day = '{:02d}'.format(d.day)
+    year = d.year
+
+
+    #check energy today month
+    std = readInflux("SELECT (fullenergy) FROM boiler WHERE time >= \'" + str(year) + "-" + month + "-" + day +"T00:00:00Z\'" + " limit 1 tz('Europe/Kiev')")
+    stm = readInflux("SELECT (fullenergy) FROM boiler WHERE time >= \'" + str(year) + "-" + month + "-01T00:00:00Z\'" + " limit 1 tz('Europe/Kiev')")
+    end = readInflux("SELECT (fullenergy) FROM boiler WHERE time <= now() ORDER BY time DESC limit 1 tz('Europe/Kiev')")
+
+    #print(std)
+
+    stdata = load_dirty_json(str(std.raw))
+    stmdata = load_dirty_json(str(stm.raw))
+    endata = load_dirty_json(str(end.raw))
+
+    value_d = int(endata['series'][0]['values'][0][1]) - int(stdata['series'][0]['values'][0][1])
+    value_m = int(endata['series'][0]['values'][0][1]) - int(stmdata['series'][0]['values'][0][1])
+    #value = int(stdata['series'][0]['values'][0][1])
+    post_2blynk('V9', str(value_d/1000))
+    post_2blynk('V10', str(value_m/1000))
+
+def readInflux(query):
+    #print(query)
+    result = ''
+    try:
+        client = InfluxDBClient(InfluxDBHost, InfluxDBPort, InfluxDBUser, InfluxDBPass, InfluxDBDatabase, timeout=10)
+    except Exception as err:
+        flash("Influx connection error: %s" % str(err))
+    if client:
+        print("Read points:")
+        try:
+            result = client.query(query)
+        except Exception as err:
+            print('InfluxDBClientError = ' + str(err))
+    return result
 
 
 
@@ -133,6 +217,10 @@ def on_subscribe(mqttc, obj, mid, granted_qos):
 
 def main():
     print("creating new instance")
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(every_minute, 'interval', seconds=10)
+    scheduler.start()
 
     mqttc = mqtt.Client()
     mqttc.on_connect = on_connect
